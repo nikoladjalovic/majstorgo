ecad6999f353b5bfa01e21797f656ba870c92685
Nikola Djalovic <djalovic.nikola99@gmail.com>
2024-11-10 15:35:43 +0100

    First commit

---
+++ src/main/java/com/djalovic/majstorgo/MajstorgoApplication.java
@@ -0,0 +1,13 @@
+package com.djalovic.majstorgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajstorgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MajstorgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/djalovic/majstorgo/MajstorgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djalovic.majstorgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajstorgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
