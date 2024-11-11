0eb59f59c3e28d4bd3e30d25f29bc41f5a61aa68
Nikola Djalovic <djalovic.nikola99@gmail.com>
2024-11-11 20:38:26 +0100

    Adding requests

--- src/main/java/com/djalovic/majstorgo/controller/MajstorController.java
+++ src/main/java/com/djalovic/majstorgo/controller/MajstorController.java
@@ -1,11 +1,29 @@
 package com.djalovic.majstorgo.controller;
 
+import com.djalovic.majstorgo.model.Majstor;
 import com.djalovic.majstorgo.service.MajstorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class MajstorController {
     @Autowired
     private MajstorService majstorService;
+
+    @GetMapping("/majstors")
+    public List<Majstor> getMajstors(){
+        return majstorService.getAllMajstors();
+    }
+
+    @PostMapping("/majstor")
+    public void addMajstor(@RequestBody Majstor majstor){
+        majstorService.addMajstor(majstor);
+    }
+
+    @PutMapping("/majstor/{id}")
+    public void updateMajstor(@RequestBody Majstor majstor, @PathVariable("id") int id){
+        majstorService.updateMajstor(majstor, id);
+    }
 }
--- src/main/java/com/djalovic/majstorgo/service/MajstorService.java
+++ src/main/java/com/djalovic/majstorgo/service/MajstorService.java
@@ -1,11 +1,42 @@
 package com.djalovic.majstorgo.service;
 
+import com.djalovic.majstorgo.model.Majstor;
 import com.djalovic.majstorgo.repository.MajstorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MajstorService {
     @Autowired
     private MajstorRepository majstorRepository;
+
+    public List<Majstor> getAllMajstors() {
+        return majstorRepository.findAll();
+    }
+
+    public void addMajstor(Majstor majstor) {
+        majstorRepository.save(majstor);
+    }
+
+    public void updateMajstor(Majstor majstor, int id) {
+        Optional<Majstor> existingMajstor = majstorRepository.findById(id);
+
+        if(existingMajstor.isPresent()){
+            Majstor majstorToUpdate = existingMajstor.get();
+            majstorToUpdate.setTypeOfMajstor(majstor.getTypeOfMajstor());
+            majstorToUpdate.setCommonHourlyRate(majstor.getCommonHourlyRate());
+            majstorToUpdate.setFirstName(majstor.getFirstName());
+            majstorToUpdate.setLastName(majstor.getLastName());
+            majstorToUpdate.setAge(majstor.getAge());
+
+            majstorRepository.save(majstorToUpdate);
+        }else{
+            System.out.println("Majstor with id " + id + "was not found and we could not update it");
+        }
+    }
+
+
 }
